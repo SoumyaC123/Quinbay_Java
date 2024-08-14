0a910601d176fbb82c3f26d73041c9284dcb9c6e
Soumya Chourasia <102222605+SoumyaC123@users.noreply.github.com>
2024-08-14 22:01:26 +0530

    Add files via upload

---
+++ ex_21_A.java
@@ -0,0 +1,11 @@
+public class ex_21_A{
+    public static void main(String [] args) {
+     int x = 1;
+     while ( x < 10 ) {
+     x = x + 1; //You can stop it from running indefinitely by adding this line.
+     if ( x > 3) { 
+     System.out.println("A");
+        }
+    }
+ }      
+}
